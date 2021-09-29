653804ba99cf8f1940401d9687984ccf674781e4
israel <israel>
2021-09-29 16:53:25 -0500

    israel

--- siteosito/apps/distribuidos/models.py
+++ siteosito/apps/distribuidos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from apps.productos.models import Producto
+from apps.proveedores.models import Proveedore
 
 # Create your models here.
+
+class Distribuido(models.Model):
+    productos =  models.ForeignKey(Producto, on_delete=models.CASCADE)
+    proveedores =  models.ForeignKey(Proveedore, on_delete=models.CASCADE)
--- siteosito/apps/productos/models.py
+++ siteosito/apps/productos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    descripcion = models.CharField(max_length=40)
+    precio = models.IntegerField()
+    numero_exit = models.IntegerField()
+    def __str__(self):
+        return self.descripcion
