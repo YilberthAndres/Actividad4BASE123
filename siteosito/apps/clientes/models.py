1986f2c9802f42e9f86156c4812372f2309afe2b
YilberthAndres <yilberthgalarza444@gmail.com>
2021-09-29 16:55:27 -0500

    yilberth

--- siteosito/apps/clientes/models.py
+++ siteosito/apps/clientes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nombre    = models.CharField(max_length=100)
-    aepllidos = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
     telefono  = models.CharField(max_length=100)
 
